64cf005f48ce2d0b72c9574a7cc97a6653ab69ab
zero_li <zhaohua_lee@163.com>
2016-10-13 10:44:01 +0800

    Initial commit

---
+++ app/src/main/java/com/zeroli/hiapp/StartActivity.java
@@ -0,0 +1,24 @@
+package com.zeroli.hiapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
